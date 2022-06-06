682e69e11007e6d2112cfc5a9ab1b691efb3c908
DJ-Llama <107007044+DJ-Llama@users.noreply.github.com>
2022-06-06 17:36:21 -0500

    Test Update

---
+++ curzon.py
@@ -0,0 +1,13 @@
+num = int(input("enter a number: "))
+
+result1 = 2 ** num + 1
+result2 = 2 * num + 1
+
+if result1 % result2 == 0:
+    print(True)
+else:
+    print(False)
+
+    "githubtest"
+
+    "test2"
